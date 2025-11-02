94c115594bc26cc80c980042bc9cafc1d7d0f051
Boluwatife Zainab Adeagbo <adeagbozain50@gmail.com>
2025-11-02 22:04:49 +0100

    Task 3

---
+++ properties/signals.py
@@ -0,0 +1,12 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from django.core.cache import cache
+from .models import Property
+
+@receiver(post_save, sender=Property)
+@receiver(post_delete, sender=Property)
+def invalidate_property_cache(sender, **kwargs):
+    """
+    Delete 'all_properties' cache when a Property is created, updated, or deleted.
+    """
+    cache.delete('all_properties')
